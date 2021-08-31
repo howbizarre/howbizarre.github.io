f64449685c4a1c9bf83731d70989a5d680930fe2
How Bizarre <howbizarre@gmail.com>
2021-08-31 17:44:54 +0300

    UPD: Towns tables

--- lots/components/lots-table.vue.js
+++ lots/components/lots-table.vue.js
@@ -1,17 +1,17 @@
 export default {
     name: "lotsTable",
-    props: ["opt", "currentWeek"],
+    props: ["opt"],
     template: `
         <table class="table table-hover border-top border-start align-middle text-center">
             <tbody>
-                <tr v-for="week in opt.weekTemplate">
+                <tr v-for="(week, index) in opt.weekTemplate">
                     <th scope="row" class="table-light border-end">
                         {{ week["short-name"] }}
                     </th>
                     <td class="border-end" :class="hour.active ? 'table-active' : ''" :key="hour.active" v-for="hour in week.hours">
                         <label class="w-100">
                             {{ hour.time }}
-                            <input type="checkbox" class="form-check-input" :name="currentWeek" :checked="hour.active" />
+                            <input type="checkbox" class="form-check-input" :name="opt.currentWeek + '_' + index + '_' + hour.time" :checked="hour.active" />
                         </label>
                     </td>
                 </tr>
